6599ecfdc5160ace5eef0c703e8840b7893fffe6
DjihaneB <bouizemdjihane1312@gmail.com>
2023-10-13 19:30:49 +0200

    Inclusion des schémas d'URL définis dans le fichier urls.py de l'application  blog  dans le schéma d'URL global de projet.

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -1,10 +1,12 @@
 
 from .views import index
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 urlpatterns = [
     path('', index, name = "index"),
+    path('blog/', include("blog.urls")),#Inclusion des schémas d'URL définis dans le fichier urls.py de l'application "blog"  dans le schéma d'URL global de votre projet
     path('admin/', admin.site.urls),
+    
 ]
