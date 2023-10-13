5914e5a01029443ee1c95adc2b827cfbcf4a0f82
DjihaneB <bouizemdjihane1312@gmail.com>
2023-10-13 18:58:22 +0200

    Création de l application blog et l ajout à la liste INSTALLED_APPS dans settings.

--- src/DocBlog/views.py
+++ src/DocBlog/views.py
@@ -1,10 +1,11 @@
 #from django.http import HttpResponse
-
+from datetime import datetime 
 from django.shortcuts import render
 
 #def index(request):
 #    return HttpResponse("<h1>Bienvenue</h1>")
 
 def index (request):
-    return render(request, "index.html")
+     
+    return render(request, "index.html", context={"date" : datetime.today() })
     
